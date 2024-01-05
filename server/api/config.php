c10a9449501fff6b312c5dc644e1247def8c0e33
Satri <67455932+satri-cn@users.noreply.github.com>
2024-01-05 14:53:13 +0800

    Update config.php

--- server/api/config.php
+++ server/api/config.php
@@ -1,20 +1,40 @@
 <?php
-    $servername="localhost";//不要动，默认为本地即可
-    $username="数据库用户名";//修改为数据库用户名
-    $password="数据库密码";//修改为数据库用户密码
-    $host = "服务端域名";//修改为域名，以“/”结尾，例如https://admin.satri.cn/
+/**
+ * 数据库连接和配置文件
+ *
+ * @version 1.0.0
+ * @author 夏创高科技软件产业（辽宁）有限公司
+ * @link https://satri.cn
+ * @license MIT
+ */
 
-    //按需设置下面
-    $tencentSecretId = "";//腾讯云账户SecretId
-    $tencentSecretKey = "";//腾讯云账户SecretKey
-    $appid = "";//微信小程序appid
-    $appsecret = "";//微信小程序appsecret
-    //创建连接
-    $conn=mysqli_connect($servername,$username,$password);
-    //检查连接
-    if(!$conn){
-        die("连接失败：".mysqli_connect_error());
-    }
-    mysqli_select_db($conn,'');//单引号''内修改为数据库名
-    //输出成功
-    // echo "连接成功";
+// 数据库连接配置
+$servername = "localhost"; // 不要动，默认为本地即可
+$username = "数据库用户名"; // 修改为数据库用户名
+$password = "数据库密码"; // 修改为数据库用户密码
+
+// 服务端配置
+$host = "服务端域名"; // 修改为域名，以“/”结尾，例如 https://admin.satri.cn/
+
+// 以下按需配置
+// 腾讯云配置
+$tencentSecretId = ""; // 腾讯云账户SecretId
+$tencentSecretKey = ""; // 腾讯云账户SecretKey
+
+// 微信小程序配置
+$appid = ""; // 微信小程序appid
+$appsecret = ""; // 微信小程序appsecret
+
+// 创建数据库连接
+$conn = mysqli_connect($servername, $username, $password);
+
+// 检查连接
+if (!$conn) {
+    die("连接失败：" . mysqli_connect_error());
+}
+
+// 选择数据库
+mysqli_select_db($conn, ''); // 单引号''内修改为数据库名
+
+// 输出成功信息
+// echo "连接成功";
